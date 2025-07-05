81a1ca0bc420d3a9711d4d0c1f3e144ece6c680f
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-05 18:35:31 +0900

    [Feat] 데이터 값 입력받는 add 함수 추가

--- src/main/java/com/house/ObejctCRUD.java
+++ src/main/java/com/house/ObejctCRUD.java
@@ -1,9 +1,52 @@
 package com.house;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ObejctCRUD implements  ICRUD{
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Object> list;
+
+    ObejctCRUD() {
+        list = new ArrayList<>();
+    }
+
     @Override
     public Object add() {
-        return null;
+        System.out.println("이름 : ");
+        String name = scanner.nextLine();
+
+        System.out.println("해당되는 카테고리\n" +
+                "1) 침대\n : " +
+                "2) 매트리스 토퍼\n : " +
+                "3) 테이블/식탁/책상\n : " +
+                "4) 소파\n : " +
+                "5) 서랍/수납장\n : " +
+                "6) 행거/옷장\n : " +
+                "7) 거울\n : " +
+                "8) 화장대/콘솔\n" +
+                "9) 유아동기구\n" +
+                "10) 야외기구\n" +
+                "11) 가벽/파티션\n");
+        int category = scanner.nextInt();
+
+        System.out.println("가로길이 입력(mm) : ");
+        int width = scanner.nextInt();
+
+        System.out.println("세로길이 입력(mm) : ");
+        int depth = scanner.nextInt();
+
+        System.out.println("높이 입력(mm) : ");
+        int height = scanner.nextInt();
+
+        System.out.println("가격 입력(원) : ");
+        int price = scanner.nextInt();
+
+        return new Object(0, name, category, width, depth, height, price);
+    }
+
+    public void addObject() {
+
     }
 
     @Override
